3c3bac8d17e83db594c15149326ff5a3b9a0b3e1
Donovan Searles <donovansearles@gmail.com>
2021-08-15 11:59:58 -0400

    added the main tf file

---
+++ main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+  }
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "replace_me"
+
+    workspaces {
+      name = "replace_me"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0c2b8ca1dad447f8a"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash 
+              sudo yum update –y 
+              sudo yum install httpd -y 
+              sudo service httpd start 
+              sudo chkconfig httpd on  
+              sudo service httpd status  
+              cd /var/www/html 
+              echo "<html><body><h1>EC2 Deployed Sucessfully</h1></body></html>"  | sudo tee index.html   
+              EOF
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "dps-ec2-sg"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
+}
