16051d5ceaae184ee30520ab79c6e11ac11143ef
Donovan Searles <donovansearles@gmail.com>
2021-08-16 23:12:15 -0400

    updated user data

--- main.tf
+++ main.tf
@@ -23,6 +23,14 @@ resource "aws_security_group" "web-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
 
 }
 
