e34873e5e10b73e31a3a3e348772141d5680edc7
Donovan Searles <donovansearles@gmail.com>
2021-08-16 23:28:41 -0400

    updated sg 3

--- main.tf
+++ main.tf
@@ -13,27 +13,27 @@ resource "aws_security_group" "web-sg" {
   name = "dps-ec2-sg"
   ingress = [
     {
-      description      = "Allow SSH"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      description = "Allow SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
 
     },
     {
-      description      = "Allow HTTP"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      description = "Allow HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
 
     },
     {
-      description      = "Allow HTTPS"
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      description = "Allow HTTPS"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
 
     }
   ]
