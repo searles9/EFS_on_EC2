1ac33adb553ef323882bad7ef6c881cab3bc07e2
Donovan Searles <donovansearles@gmail.com>
2021-08-16 23:15:47 -0400

    terraform fmt

--- main.tf
+++ main.tf
@@ -25,10 +25,10 @@ resource "aws_security_group" "web-sg" {
   }
   egress = [
     {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   ]
 
